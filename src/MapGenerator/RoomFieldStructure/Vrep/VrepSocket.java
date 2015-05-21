ec5b06ecb3632dc3db547ce1b1c8f798de8c3b9e
Lukas Jelinek <lukas.jelinek1@gmail.com>
2015-05-21 21:20:55 +0200

    Socket communication with Vrep added

---
+++ src/MapGenerator/RoomFieldStructure/Vrep/VrepSocket.java
@@ -0,0 +1,56 @@
+package MapGenerator.RoomFieldStructure.Vrep;
+
+
+import MapGenerator.RoomFieldStructure.Map;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class VrepSocket {
+    private static int PORT;
+    private DataOutputStream out_to_server;
+    private boolean is_connected = false;
+    private String data;
+    Map map;
+    Socket output_socket;
+
+    public VrepSocket(Map map, int PORT) {
+        this.map = map;
+        this.PORT = PORT;
+    }
+
+    public  void addData(String txt){
+        data = txt;
+    }
+
+    private void sendBlock(String txt) throws IOException {
+       out_to_server.writeBytes(txt);
+
+    }
+
+    public void connect() throws IOException {
+        is_connected=false;
+        output_socket = new Socket("localhost", PORT);
+        out_to_server = new DataOutputStream(output_socket.getOutputStream());
+        is_connected=true;
+
+    }
+
+    public void close() throws IOException {
+        output_socket.close();
+        is_connected=false;
+    }
+
+    public void send() throws IOException {
+        if(!is_connected) connect();
+        if(is_connected && data.length() != 0){
+            sendBlock(data);
+        }
+    }
+
+    public void sendMap() throws IOException {
+        addData(map.printMapaData());
+        send();
+    }
+}
