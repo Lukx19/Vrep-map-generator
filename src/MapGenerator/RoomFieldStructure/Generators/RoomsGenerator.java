e8fbe563279aee2a7425109202e0144178851fd3
Lukas Jelinek <lukas.jelinek1@gmail.com>
2015-05-21 23:05:55 +0200

    General graph implementation

--- src/MapGenerator/RoomFieldStructure/Generators/RoomsGenerator.java
+++ src/MapGenerator/RoomFieldStructure/Generators/RoomsGenerator.java
@@ -30,6 +30,20 @@ public class RoomsGenerator {
 
     }
 
+    public RoomsGenerator(Random rand){
+        this.rand = rand;
+        this.width = 20;
+        this.height = 20;
+        this.room_count = 5;
+        room_types = new ArrayList<>();
+        room_types.add(new RoomType(4,4));
+        room_types.add(new RoomType(3,5));
+        room_types.add(new RoomType(5,3));
+        room_types.add(new RoomType(6,4));
+        room_types.add(new RoomType(4,6));
+        room_types.add(new RoomType(6,6));
+    }
+
     public void generateRooms(){
         for(int i = 0; i<room_count;++i){
             // place room somewhere inside room 0 in some distance from edges of this environmental room
@@ -44,5 +58,29 @@ public class RoomsGenerator {
             }
 
         }
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public void setMap(Map map) {
+        this.map = map;
+    }
+
+    public void setRoom_count(int room_count) {
+        this.room_count = room_count;
+    }
+
+    public void addRoomType(int width,int height){
+        room_types.add(new RoomType(width,height));
+    }
+
+    public void clearRoomTypes() {
+        room_types.clear();
     }
 }
