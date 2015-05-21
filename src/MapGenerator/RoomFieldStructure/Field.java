51eb982d10563c052087c14f27f5fdaa27cd5448
Lukas Jelinek <lukas.jelinek1@gmail.com>
2015-05-21 13:52:34 +0200

    Room generator implemented

--- src/MapGenerator/RoomFieldStructure/Field.java
+++ src/MapGenerator/RoomFieldStructure/Field.java
@@ -7,24 +7,29 @@ import java.util.PriorityQueue;
 
 public class Field {
     public enum State{WALL,EMPTY,IN_ROOM,DOOR}
+    public enum Wall{HORIZONTAL_LINE,VERTICAL_LINE,HORIZONTAL_WITH_TOP,HORIZONTAL_WITH_BOTTOM,
+        VERTICAL_WITH_RIGHT,VERTICAL_WITH_LEFT,
+        LEFT_TOP_CORNER,RIGHT_TOP_CORNER,RIGHT_BOTTOM_CORNER,
+        LEFT_BOTTOM_CORNER,CROSS}
     private Map map;
     private State state;
+    private Wall wall_type;
     private int posX;
     private int posY;
     private ArrayList<Room> unique_neighbours;
-    private Room[] neighbours;
-    private PriorityQueue<Room> owners; // 0 - northWest 1- north 2 -northEast 3-eats 4-SouthEast 5-south
-    // 6- southWest 7-west
+    private Room[] neighbours; // 0 - northWest 1 -northEast 3-SouthEast 4-southWest 5-north 6 east 7south 8 west
+    private PriorityQueue<Room> owners;
+
 
     public Field(Map m,int x,int y,int level) {
         this.map=m;
         posX=x;
         posY=y;
         //this.level=level;
-        neighbours = new Room[4];
+        neighbours = new Room[8];
         state=State.EMPTY;
         unique_neighbours=new ArrayList<>();
-        owners = new PriorityQueue<>(4, (r1, r2) -> (r1.getLevel() < r2.getLevel())?-1:1);
+        owners = new PriorityQueue<>(4, (r1, r2) -> (r1.getLevel() > r2.getLevel())?-1:1);
 
     }
 
@@ -39,6 +44,9 @@ public class Field {
         if(owner != null && !owners.contains(owner)){
             owners.add(owner);
         }
+    }
+    public Room getNeighbour(int i){
+        return neighbours[i];
     }
 
     public void removeOwner(Room room) {
@@ -59,13 +67,17 @@ public class Field {
         state= s;
     }
 
+    public Wall getWallType(){
+        return wall_type;
+    }
+
     public int getNeighboursCount(){
         return unique_neighbours.size();
     }
 
-    public Room[] getNeighboursGrid(){
-        return neighbours;
-    }
+   // public Room[] getNeighboursGrid(){
+  //      return neighbours;
+  //  }
 
     public ArrayList<Room> getUnique_neighbours(){
         return unique_neighbours;
@@ -73,42 +85,82 @@ public class Field {
 
     public void calcState(){
         prepareData();
-        if(unique_neighbours.size() == 1){
+        if(neighbours[0] == neighbours[1] && neighbours[1]== neighbours[2] && neighbours[2]==neighbours[3]
+                && neighbours[0]==neighbours[3]){
             // this field is inside of the room
             state=State.IN_ROOM;
         }else if(unique_neighbours.size() > 1){
             // this field has multiple owners -> this field is at the boundary of rooms
             state= State.WALL;
+            calcWallType();
         }else{
             // no owner of this field
             state= State.EMPTY;
         }
     }
 
+    private void calcWallType() {
+        int[] wall_parts = new int[4]; // 0-top,1-right , 2-bottom 3-left
+        for(int i=0;i<4;++i){
+            if(neighbours[i] != neighbours[(i+1)%4])
+                wall_parts[i]=1;
+            else
+                wall_parts[i]=0;
+        }
+        int code = wall_parts[0]*1000+wall_parts[1]*100+wall_parts[2]*10+wall_parts[3];
+        switch(code){
+            case 1010: wall_type=Wall.VERTICAL_LINE; break;
+            case 101: wall_type=Wall.HORIZONTAL_LINE; break;
+            case 1111: wall_type=Wall.CROSS; break;
+            case 111: wall_type=Wall.HORIZONTAL_WITH_BOTTOM; break;
+            case 1101: wall_type=Wall.HORIZONTAL_WITH_TOP; break;
+            case 1011: wall_type=Wall.VERTICAL_WITH_LEFT; break;
+            case 1110: wall_type=Wall.VERTICAL_WITH_RIGHT; break;
+            case 1001: wall_type=Wall.LEFT_TOP_CORNER; break;
+            case 1100: wall_type=Wall.RIGHT_TOP_CORNER; break;
+            case 11: wall_type=Wall.LEFT_BOTTOM_CORNER; break;
+            case 110: wall_type=Wall.RIGHT_BOTTOM_CORNER; break;
+        }
+    }
 
 
     private void prepareData(){
-        if(map.getField(posX-1,posY-1) != null) neighbours[0]=map.getField(posX-1,posY-1).getTopOwner();
-        else neighbours[0]=null;
-        if(map.getField(posX,posY-1) != null)neighbours[1]=map.getField(posX,posY-1).getTopOwner();
-        else neighbours[1]=null;
-        if(map.getField(posX+1,posY-1) != null)neighbours[2]=map.getField(posX+1,posY-1).getTopOwner();
+//        if(map.getField(posX-1,posY-1) != null) neighbours[0]=map.getField(posX-1,posY-1).getTopOwner();
+//        else neighbours[0]=null;
+//        if(map.getField(posX+1,posY-1) != null)neighbours[1]=map.getField(posX+1,posY-1).getTopOwner();
+//        else neighbours[1]=null;
+//        if(map.getField(posX+1,posY+1) != null)neighbours[2]=map.getField(posX+1,posY+1).getTopOwner();
+//        else neighbours[2]=null;
+//        if(map.getField(posX-1,posY+1) != null)neighbours[3]=map.getField(posX-1,posY+1).getTopOwner();
+//        else neighbours[3]=null;
+        unique_neighbours.clear();
+        if(map.getField(posX,posY-1) != null){
+            neighbours[0]=map.getField(posX,posY-1).getNeighbour(3);
+            neighbours[1]=map.getField(posX,posY-1).getNeighbour(2);
+        }
+        else{
+            neighbours[0]=null;
+            neighbours[1]=null;
+        }
+        if(map.getField(posX+1,posY+1) != null) neighbours[2]=map.getField(posX+1,posY+1).getTopOwner();
         else neighbours[2]=null;
-        if(map.getField(posX-1,posY-1) != null)neighbours[3]=map.getField(posX+1,posY).getTopOwner();
+        if(map.getField(posX-1,posY) != null) neighbours[3]=map.getField(posX-1,posY).getNeighbour(2);
         else neighbours[3]=null;
-        if(map.getField(posX+1,posY+1) != null)neighbours[4]=map.getField(posX+1,posY+1).getTopOwner();
+
+        if(map.getField(posX,posY-1) != null) neighbours[4]=map.getField(posX,posY-1).getTopOwner();
         else neighbours[4]=null;
-        if(map.getField(posX,posY+1) != null)neighbours[5]=map.getField(posX,posY+1).getTopOwner();
+        if(map.getField(posX+1,posY) != null) neighbours[5]=map.getField(posX+1,posY).getTopOwner();
         else neighbours[5]=null;
-        if(map.getField(posX-1,posY+1) != null)neighbours[6]=map.getField(posX-1,posY+1).getTopOwner();
+        if(map.getField(posX,posY+1) != null) neighbours[6]=map.getField(posX,posY+1).getTopOwner();
         else neighbours[6]=null;
-        if(map.getField(posX-1,posY) != null)neighbours[7]=map.getField(posX-1,posY).getTopOwner();
+        if(map.getField(posX-1,posY) != null) neighbours[7]=map.getField(posX-1,posY).getTopOwner();
         else neighbours[7]=null;
 
-        // calculates how many unique neighbours are at one field
+       // calculates how many unique neighbours are at one field
        unique_neighbours = new ArrayList<>();
-        for(int i =0;i<8;i+=2){
+        for(int i =0;i<4;++i){
             final int finalI = i;
+            //if this neighbor is not in our unique list we add it
             if(unique_neighbours.stream().filter(c->c == neighbours[finalI]).count() == 0)
                 unique_neighbours.add(neighbours[i]);
         }
@@ -145,5 +197,35 @@ public class Field {
             return higher_room;
     }
 
+    public String printCoords(){
+        return "["+getX()+","+getY()+"] ";
+    }
 
+    @Override
+    public String toString() {
+        switch(state){
+            case IN_ROOM:
+            case EMPTY: return "   ";
+            case WALL:
+                switch (wall_type){
+                    case LEFT_TOP_CORNER: return "-' ";
+                    case RIGHT_TOP_CORNER: return " '-";
+                    case RIGHT_BOTTOM_CORNER: return " ,-";
+                    case LEFT_BOTTOM_CORNER:  return "-, ";
+                    case CROSS: return "-|-";
+                    case HORIZONTAL_LINE: return "---";
+                    case VERTICAL_LINE: return " | ";
+                    case HORIZONTAL_WITH_BOTTOM: return "-,-";
+                    case HORIZONTAL_WITH_TOP: return "-'-";
+                    case VERTICAL_WITH_LEFT: return "-| ";
+                    case VERTICAL_WITH_RIGHT: return " |-";
+                    default: return "UNKNOWN";
+                }
+            case DOOR: return " D ";
+            default: return "UNKNOWN";
+        }
+       //return owners.size() >0 ?String.valueOf(this.getTopOwner().getRoomID()): " ";
+       //return String.valueOf(getUnique_neighbours().size());
+
+    }
 }
---
+++ src/MapGenerator/RoomFieldStructure/Generators/RoomsGenerator.java
@@ -0,0 +1,48 @@
+package MapGenerator.RoomFieldStructure.Generators;
+
+
+import MapGenerator.RoomFieldStructure.Map;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class RoomsGenerator {
+    Map map;
+    Random rand;
+    int width;
+    int height;
+    int room_count;
+    ArrayList<RoomType> room_types;
+
+    public RoomsGenerator(Map map, Random rand, int width, int height, int room_count) {
+        this.map = map;
+        this.rand = rand;
+        this.width = width;
+        this.height = height;
+        this.room_count = room_count;
+        room_types = new ArrayList<>();
+        room_types.add(new RoomType(4,4));
+        room_types.add(new RoomType(3,5));
+        room_types.add(new RoomType(5,3));
+        room_types.add(new RoomType(6,4));
+        room_types.add(new RoomType(4,6));
+        room_types.add(new RoomType(6,6));
+
+    }
+
+    public void generateRooms(){
+        for(int i = 0; i<room_count;++i){
+            // place room somewhere inside room 0 in some distance from edges of this environmental room
+            int place_x=rand.nextInt(width-4)+2;
+            int place_y = rand.nextInt(height-4)+2;
+            RoomType type = room_types.get(rand.nextInt(room_types.size()));
+            if(place_x+type.getWidth() >width-2 || place_y+type.getHeight() > height-2){
+                // selected room on selected position is oversize - repeat loop
+                --i;
+            }else{
+                map.addRoom(place_x,place_y,type.getWidth(),type.getHeight());
+            }
+
+        }
+    }
+}
